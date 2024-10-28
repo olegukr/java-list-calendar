34cd30409d1b1ee718fe96dbe0cb9261dd6fa4a9
olegukr <oleg.poberezhets@gmail.com>
2024-10-28 10:49:11 +0100

    add getDays getSize

---
+++ src/main/java/factoriaf5/exercise/WeekDay.java
@@ -0,0 +1,52 @@
+package factoriaf5.exercise;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeekDay {
+
+    public List<String> days;
+
+    public WeekDay() {
+        createList();
+    }
+
+    public void createList() {
+        days = new ArrayList<String>();
+        days.add("Monday");
+        days.add("Tuesday");
+        days.add("Wednesday");
+        days.add("Thursday");
+        days.add("Friday");
+        days.add("Saturday");
+        days.add("Sunday");
+        
+            // DaysOfWeek.
+
+        // String[] daysOfWeek = 
+        // {
+        //     "Monday", 
+        //     "Tuesday", 
+        //     "Wednesday", 
+        //     "Thursday", 
+        //     "Friday", 
+        //     "Saturday", 
+        //     "Sunday", 
+        // };
+    }
+
+    // public void render() {
+    //     System.out.println(days);
+    // }
+
+    public List<String> getDays() {
+        return days;
+    }
+
+    public int getSizeList() {
+        return days.size();
+    }
+
+    public void setDays(List<String> days) {
+        this.days = days;
+    }
+}
---
+++ src/test/java/factoriaf5/exercise/WeekDayTest.java
@@ -0,0 +1,62 @@
+package factoriaf5.exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WeekDayTest {
+
+    private WeekDay weekDay;
+
+    // @BeforeEach
+    // public void setUp() {
+    //     WeekDay weekDay = new WeekDay();
+    // }
+
+    @Test
+    @DisplayName("Test getDaysOfWeek()")
+    void testGetDaysOfWeek() {
+        // test Object type WeekDay
+        WeekDay weekDay = new WeekDay();
+
+        //Given
+        ArrayList<String> daysExpected = new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+
+        //When
+        ArrayList<String> result = (ArrayList<String>) weekDay.getDays();
+
+        //Then
+        assertEquals(result, daysExpected);
+
+    }
+
+    @Test
+    @DisplayName("Test getSizeList()")
+    void testgetSizeList() {
+        WeekDay weekDay = new WeekDay();
+        assertEquals(weekDay.getSizeList(), 7);
+    }
+
+//     @Test
+//     void testSetDaysOfWeek() {
+
+//     }
+//         @Test
+//     @DisplayName("Devuelve Fizz si el número es divisible por 5")
+//     void testIfdivisibleBy5_ReturnBuzz() {
+//         // test Object type Fizzbuzz Inisialisation
+        
+//         //Given
+//         int numb = 5;
+
+//         //When
+//         String result = fizzbuzz.checkNumber(numb);
+
+//         //Then
+//         assertThat(fizzbuzz, instanceOf(FizzBuzz.class));
+//         assertThat(result,is("Buzz"));
+// }
+}
